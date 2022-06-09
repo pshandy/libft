b311ac085678048798869a95599477a4174b47dd
pshandy <92032776+pshandy@users.noreply.github.com>
2022-06-09 19:53:03 +0300

    Add files via upload

---
+++ libft/ft_strstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pshandy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/14 12:06:35 by pshandy           #+#    #+#             */
+/*   Updated: 2021/10/14 12:06:36 by pshandy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+ * char *ft_strstr(char *str, char *to_find)
+ *
+ * Returns a pointer to the first occurrence of str2 in str1, 
+ * or a null pointer if str2 is not part of str1.
+ */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			++j;
+		}
+		++i;
+	}
+	return (NULL);
+}
